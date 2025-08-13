df8653f85e2731dfb28e2173e49e0d6dcea1d063
Elias Sarband <elias.sarband@gmail.com>
2025-08-13 16:11:59 +0330

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpyGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
